b62b23465e10adde2718210c5933e8da295cb263
Gabriel Arcos <gabbo0709@gmail.com>
2024-03-22 03:51:02 -0600

    Se inicializa el proyecto de .NET MAUI para la aplicación.

---
+++ app/Flecha Digital/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Flecha_Digital
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new AppShell();
+		}
+	}
+}
---
+++ app/Flecha Digital/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Flecha_Digital
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
