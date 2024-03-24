dbe9e16ff760b5f476bba0828fcfc98f3cbd869d
Eric Gustavo González García <48300855+Gus524@users.noreply.github.com>
2024-03-23 22:14:11 -0600

    Actualización en tablas y primeros INSERTS
    Se actualizó la tabla de camión para agregar la columna no_de camión y agregar id_camión como llave primaria, se actualizaron sus referencias foráneas 
    Se agregaron los primeros INSERT que corresponden a servicios, y tipo de viaje

--- base de datos/db_flecha.sql
+++ base de datos/db_flecha.sql
@@ -43,8 +43,9 @@ CREATE TABLE Linea(
 )
 
 CREATE TABLE Camion(
-	no_camion		INT PRIMARY KEY NOT NULL,
+	id_camion		INT PRIMARY KEY NOT NULL,
 	cve_linea		INT FOREIGN KEY (cve_linea) REFERENCES Linea(cve_linea) NOT NULL,
+	no_camion		INT NOT NULL,
 	cant_asientos	INT NOT NULL
 )
 
@@ -63,7 +64,7 @@ CREATE TABLE Tipo_Asiento(
 CREATE TABLE Asiento(
 	no_asiento		INT PRIMARY KEY NOT NULL,
 	cve_estado		INT FOREIGN KEY (cve_estado) REFERENCES Estado_Asiento(cve_estado) NOT NULL,
-	no_camion		INT FOREIGN KEY (no_camion) REFERENCES Camion(no_camion) NOT NULL,
+	id_camion		INT FOREIGN KEY (id_camion) REFERENCES Camion(id_camion) NOT NULL,
 	cve_tipo		INT FOREIGN KEY (cve_tipo) REFERENCES Tipo_Asiento(cve_tipo) NOT NULL
 )
 
@@ -71,14 +72,15 @@ CREATE TABLE Asiento(
 --Local:  La ruta de este autobús comienza en esta terminal y/o central.
 CREATE TABLE Tipo_Viaje(
 	cve_tipo		INT PRIMARY KEY IDENTITY (1,1) NOT NULL,
-	descripcion		NVARCHAR(128) NOT NULL
+	tipo			NVARCHAR(128) NOT NULL,
+	descripcion		NVARCHAR(128)
 )
 
 CREATE TABLE Viaje(
 	no_servicio		INT PRIMARY KEY NOT NULL,
 	origen			INT FOREIGN KEY (origen) REFERENCES Central(cve_central) NOT NULL,
 	destino			INT FOREIGN KEY (destino) REFERENCES Central(cve_central) NOT NULL,
-	no_camion		INT FOREIGN KEY (no_camion) REFERENCES Camion(no_camion) NOT NULL,
+	id_camion		INT FOREIGN KEY (id_camion) REFERENCES Camion(id_camion) NOT NULL,
 	cve_tipo		INT FOREIGN KEY (cve_tipo) REFERENCES Tipo_Viaje(cve_tipo) NOT NULL,
 	fecha_salida	DATE NOT NULL,
 	hora_salida		TIME NOT NULL,
@@ -86,7 +88,6 @@ CREATE TABLE Viaje(
 	fecha_llegada	DATE NOT NULL,
 	hora_llegada	TIME NOT NULL	
 )
-
 
 CREATE TABLE Usuario(
 	id_usuario		INT PRIMARY KEY IDENTITY (1,1),
@@ -98,14 +99,12 @@ CREATE TABLE Usuario(
 	tel_user		NUMERIC(12) NOT NULL
 )
 
-
 CREATE TABLE Operacion(
 	no_operacion	INT PRIMARY KEY IDENTITY (1,1) NOT NULL,
 	id_usuario		INT FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario) NOT NULL,
 	cant_boletos	INT NOT NULL,
 	costo_total		MONEY NOT NULL
 )
-
 
 CREATE TABLE Estado_Boleto(
 	cve_estado		INT PRIMARY KEY IDENTITY NOT NULL,
@@ -127,6 +126,44 @@ CREATE TABLE Boleto(
 	tel_cliente		NUMERIC(12) NOT NULL,
 	costo			MONEY NOT NULL
 )
+
+INSERT INTO Clase(descripcion) VALUES
+('Economico'),
+('SEMI_DIRECTA'),
+('Ejecutivo'),
+('Primer Select')
+
+INSERT INTO Servicio(descripcion) VALUES
+('No hay servicios disponibles'),
+('Aire acondicionado'),
+('Toma corriente'),
+('Sanitario mixto'),
+('Semicama'),
+('Luz de lectura'),
+('Pantalla genera'),
+('Wi-Fi')
+
+INSERT INTO Clase_Servicio (cve_clase, cve_servicio) VALUES 
+(1, 1),
+(2, 1),
+(3, 2),
+(3, 3),
+(3, 4),
+(3, 5),
+(3, 6),
+(3, 7),
+(3, 8),
+(4, 2),
+(4, 3),
+(4, 4),
+(4, 5),
+(4, 6),
+(4, 7),
+(4, 8)
+
+INSERT INTO Tipo_Viaje(tipo, descripcion) VALUES
+('Local', 'La ruta de este autobús comienza en esta terminal y/o central.'),
+('Paso', 'La ruta de este autobús comenzó en otra terminal y/o central, es decir, el autobús realiza una parada en este origen.')
 
 ALTER AUTHORIZATION ON DATABASE::db_flecha TO sa
 
