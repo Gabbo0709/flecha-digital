db155edc3ff11edcf94914c65c1706b88e79eb3c
Gabriel Arcos <gabbo0709@gmail.com>
2024-03-24 18:33:52 -0600

    Se crean los directorios de los microservicios definidos. En cada uno de estos, se añade su respectivo Dockerfile. Se añaden los contenedores de los microservicios al compositor de docker.

--- base de datos/Joins.sql
+++ base de datos/Joins.sql
@@ -1,9 +1,9 @@
-Use db_flecha 
+Use db_flecha
 GO
 
-SELECT descripcion_clase, descripcion_servicio FROM Clase AS c 
+SELECT descripcion_clase, descripcion_servicio FROM Clase AS c
 	INNER JOIN Clase_Servicio AS cs
-	ON c.cve_clase = cs.cve_clase 
+	ON c.cve_clase = cs.cve_clase
 	INNER JOIN Servicio AS s
 	ON s.cve_servicio = cs.cve_servicio;
 
