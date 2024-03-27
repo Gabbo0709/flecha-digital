c5a35536feace69affb0db37547f6b113e40128c
Gabriel Arcos <gabbo0709@gmail.com>
2024-03-27 03:40:54 -0600

    Se añade using de Http a la aplicación de .NET Maui

--- app/Flecha Digital/GlobalUsings.cs
+++ app/Flecha Digital/GlobalUsings.cs
@@ -7,3 +7,4 @@ global using System.Collections.ObjectModel;
 global using System.Diagnostics;
 global using System.Runtime.CompilerServices;
 global using System.Text.Json;
+global using System.Net.Http;
