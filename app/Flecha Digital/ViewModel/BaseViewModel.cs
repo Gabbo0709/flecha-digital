7cc0bfb06fb7bb8ef0237d3132b2166b0b449bb6
Eric Gustavo González García <e.gus.gg@gmail.com>
2024-03-28 12:49:29 -0600

    base MVVM
    Se agregó el BaseViewModel.cs (Base para MVVM)

---
+++ app/Flecha Digital/ViewModel/BaseViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Flecha_Digital.ViewModel;
+
+public partial class BaseViewModel : ObservableObject
+{
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+    bool isBusy;
+
+    [ObservableProperty]
+    string title;
+
+    public bool IsNotBusy => !IsBusy;
+    public BaseViewModel()
+    {
+    }
+}
+
