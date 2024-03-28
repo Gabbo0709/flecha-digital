c611cd89671795621e23add37d21fd880dd17e60
Gabriel Arcos <gabbo0709@gmail.com>
2024-03-27 22:39:40 -0600

    Revisión de microservicio boletos

--- server/services/boletos.api/models/classes/boletos-repositorio.js
+++ server/services/boletos.api/models/classes/boletos-repositorio.js
@@ -2,7 +2,14 @@ const dao = require('../../data/dao');
 const Boleto = require('./boleto');
 
 class BoletosRepositorio {
-    async buscarBoleto(boleto){
+
+    /**
+     * @param {Boleto} boleto
+     * @returns {Boleto}
+     * @description Busca un boleto en la base de datos por su número de boleto
+     */
+
+    static async buscarBoleto(boleto){
         if(!(boleto instanceof Boleto)) {
             return null;
         }
@@ -11,30 +18,20 @@ class BoletosRepositorio {
         return result != null && result.length > 0 ? new Boleto(result[0]) : null;
     }
 
-    async buscarBoletos(boleto){
+
+    /**
+     * @param {Boleto} boleto
+     * @returns {Promise<Boleto[]>}
+     * @description Busca boletos en la base de datos por su número de operación
+     */
+
+    static async buscarBoletos(boleto){
         if(!(boleto instanceof Boleto)) {
             return null;
         }
         let query = `SELECT * FROM Boleto WHERE no_operacion = ${boleto.no_operacion}`;
         let result = await dao.consultar(query);
-        return result != null && result.length > 0 ? new Boleto(result) : null;
-    }
-    
-    async buscarBoletoToken(boleto){
-        if(!(boleto instanceof Boleto)) {
-            return null;
-        }
-        let query = `SELECT * FROM Boleto WHERE token_fac = '${boleto.token_fac}'`;
-        let result = await dao.consultar(query);
-        return result != null && result.length > 0 ? new Boleto(result[0]) : null;
-    }
-    
-    async actualizarEstadoBoleto(boleto, estado){
-        if(!(boleto instanceof Boleto)) {
-            return false;
-        }
-        let query = `UPDATE Boleto SET cve_estado = ${estado} WHERE no_boleto = ${boleto.no_boleto}`;
-        return await dao.ejecutarQuery(query);
+        return result.map(boleto => new Boleto(boleto));
     }
 }
 
