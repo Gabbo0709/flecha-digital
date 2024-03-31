268a51013f4ba7d275014afbc3a502ae7783374d
Gabriel Arcos <cbi2223046662@izt.uam.mx>
2024-03-31 15:42:26 -0600

    Pruebas de central
    Se modidfica la obtencion de centrales del repositorio
    
    Se prueba de forma exitosa
    
    Se modifica el modelo de activdad, añadiendo la fecha de salida y la fecha de llegada

--- server/services/usuarios.api/models/classes/actividad.js
+++ server/services/usuarios.api/models/classes/actividad.js
@@ -10,6 +10,8 @@ class Actividad {
         this.no_operacion = actividad.no_operacion;
         this.origen_viaje = actividad.origen_viaje;
         this.destino_viaje = actividad.destino_viaje;
+        this.fecha_salida = actividad.fecha_salida;
+        this.fecha_llegada = actividad.fecha_llegada;
         this.cantidad_boletos = actividad.cantidad_boletos;
     };
 }
--- server/services/usuarios.api/tests/obtenerCentrales.test.js
+++ server/services/usuarios.api/tests/obtenerCentrales.test.js
@@ -1,1 +1,11 @@
-const Central = 
+const Central = require('../models/classes/central');
+const CentralesRepositorio = require('../models/classes/centrales-repositorio');
+
+describe('Test de obtener centrales', () => {
+    test('Obtener centrales', async () => {
+        const result = await CentralesRepositorio.obtenerCentrales();
+        expect(result).not.toBeNull();
+        expect(result).toBeInstanceOf(Array); // Array de objetos Central
+        console.log(result);
+    });
+});
