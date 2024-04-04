1eb987a002d0dc1091c442627cb5e819981f4519
Gabriel Arcos <cbi2223046662@izt.uam.mx>
2024-04-04 01:16:14 -0600

    Boleto Apple Wallet
    Se implementó la creación de boleto de Apple Wallet. Falta mapear el microservidor

--- server/services/boleto-apple-wallet.api/Modelos/Boleto.cs
+++ server/services/boleto-apple-wallet.api/Modelos/Boleto.cs
@@ -1,6 +1,63 @@
-﻿namespace boleto_apple_wallet.api.Modelos
+﻿using System.Text.Json;
+
+namespace boleto_apple_wallet.api.Modelos;
+public partial class Boleto
 {
-	public class Boleto
+	public string? ViajeId { get; set; }
+	public string? BoletoId { get; set; }
+	public short Estado { get; set; }
+	public string? NombrePasajero { get; set; }
+	public int Asiento { get; set; }
+	public string? Categoria { get; set; }
+	public string? Origen { get; set; }
+	public string? Destino { get; set; }
+	public string? NumeroServicio { get; set; }
+	public string? FechaHoraSalida { get; set; } // Formato: "dd-MM-YYYY HH:mm"
+	public string? PuertaEmbarque { get; set; }
+	public string? TokenFacturacion { get; set; }
+	public string? NumeroOperacion { get; set; }
+	public float TotalPago { get; set; }
+	public string? MetodoPago { get; set; }
+
+	public Boleto(string? viajeId, string? boletoId, short estado, string? nombrePasajero, int asiento, string? categoria, string? origen, string? destino, string? numeroServicio, string? fechaHoraSalida, string? puertaEmbarque, string? tokenFacturacion, string? numeroOperacion, float totalPago, string? metodoPago)
 	{
+		ViajeId = viajeId;
+		BoletoId = boletoId;
+		Estado = estado;
+		NombrePasajero = nombrePasajero;
+		Asiento = asiento;
+		Categoria = categoria;
+		Origen = origen;
+		Destino = destino;
+		NumeroServicio = numeroServicio;
+		FechaHoraSalida = fechaHoraSalida;
+		PuertaEmbarque = puertaEmbarque;
+		TokenFacturacion = tokenFacturacion;
+		NumeroOperacion = numeroOperacion;
+		TotalPago = totalPago;
+		MetodoPago = metodoPago;
+	}
+	// Sobrecarga de constructor para deserializar JSON
+	public Boleto(string? json)
+	{
+		if (json != null)
+		{
+			var boleto = JsonSerializer.Deserialize<Boleto>(json);
+			ViajeId = boleto.ViajeId ?? "-1";
+			BoletoId = boleto.BoletoId ?? "-1";
+			Estado = boleto.Estado;
+			NombrePasajero = boleto.NombrePasajero ?? "Nombre Pasajero";
+			Asiento = boleto.Asiento;
+			Categoria = boleto.Categoria ?? "Categoria";
+			Origen = boleto.Origen ?? "Origen";
+			Destino = boleto.Destino ?? "Destino";
+			NumeroServicio = boleto.NumeroServicio ?? "Numero Servicio";
+			FechaHoraSalida = boleto.FechaHoraSalida ?? "dd-MM-YYYY HH:mm";
+			PuertaEmbarque = boleto.PuertaEmbarque ?? "Puerta Embarque";
+			TokenFacturacion = boleto.TokenFacturacion ?? "Token Facturacion";
+			NumeroOperacion = boleto.NumeroOperacion ?? "Numero Operacion";
+			TotalPago = boleto.TotalPago;
+			MetodoPago = boleto.MetodoPago ?? "Metodo Pago";
+		}
 	}
 }
---
+++ server/services/boleto-apple-wallet.apiTests/APIs/CrearPassTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using boleto_apple_wallet.api.APIs
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boleto_apple_wallet.api.APIs.Tests
+{
+	[TestClass()]
+	public class CrearPassTests
+	{
+		[TestMethod()]
+		public void CrearNuevoApplePassTest()
+		{
+
+			Assert.Fail();
+		}
+	}
+}
